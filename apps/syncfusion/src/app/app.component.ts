46b0a708cae00527d9972568a19bfbcc477d64d0
NguyenCongNghiep97 <nghiep@innomizetech.com>
2021-09-08 09:31:10 +0700

    First nx commit

--- apps/syncfusion/src/app/app.component.ts
+++ apps/syncfusion/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sampleData } from './datasource';
 
 @Component({
   selector: 'learning-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'syncfusion';
+export class AppComponent implements OnInit {
+  public data: Object[] = [];
+  ngOnInit(){
+    this.data = sampleData;
+  }
 }
--- apps/syncfusion/src/app/app.module.ts
+++ apps/syncfusion/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TreeGridAllModule } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TreeGridAllModule],
   providers: [],
   bootstrap: [AppComponent],
 })
